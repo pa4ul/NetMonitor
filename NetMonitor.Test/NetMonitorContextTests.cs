02d8ba66e21d9ac14d3a971b8a1d797a9c3affea
pa4ul <paulherbich1@gmail.com>
2023-01-09 12:53:54 +0100

    seed

--- NetMonitor.Test/NetMonitorContextTests.cs
+++ NetMonitor.Test/NetMonitorContextTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using NetMonitor.Infrastructure;
+using NetMonitor.Model;
 
 namespace NetMonitor.Test;
 
@@ -23,5 +24,23 @@ public class NetMonitorContextTests
     public void CreateDatabaseSuccessTest()
     {
         using var db = GetDatabase(deleteDb: true);
+        
+    }
+
+    [Fact]
+    public void seed_database()
+    {
+        using var db = GetDatabase(true);
+
+        MonitorInstance m1 = new MonitorInstance("HTL Spengergasse");
+
+        Host h1 = new Host("Cisco Server 1", "192.168.0.10", new Description("Cisco Server at B.3.10"));
+        
+        
+        db.MonitorInstances.Add(m1);
+        db.Hosts.Add(h1);
+        m1.AddHost(h1);
+
+        db.SaveChanges();
     }
 }
