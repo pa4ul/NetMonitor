8db172ddbb56a2e86053492ef9f46f6809fbd9a4
pa4ul <paulherbich1@gmail.com>
2023-04-04 22:10:23 +0200

    Login rework

--- NetMonitor.Application/Dto/MonitorInstanceDto.cs
+++ NetMonitor.Application/Dto/MonitorInstanceDto.cs
@@ -1,3 +1,5 @@
+using NetMonitor.Model;
+
 namespace NetMonitor.Dto;
 
-public record MonitorInstanceDto(string Name, List<HostDto> Hosts, Guid Guid);
+public record MonitorInstanceDto(string Name, List<HostDto> Hosts, Guid Guid, User Manager);
--- NetMonitor.Webapp/Pages/Index.cshtml.cs
+++ NetMonitor.Webapp/Pages/Index.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using NetMonitor.Dto;
 using NetMonitor.Infrastructure;
 using NetMonitor.Model;
+using NetMonitor.Webapp.Services;
 using Host = NetMonitor.Model.Host;
 
 namespace NetMonitor.Webapp.Pages;
@@ -11,19 +12,24 @@ namespace NetMonitor.Webapp.Pages;
 public class IndexModel : PageModel
 {
     private readonly NetMonitorContext _db;
-    public List<MonitorInstanceDto> MonitorInstances { get; private set; } = new();
-
-    public IndexModel(NetMonitorContext db)
+    private readonly AuthService _authService;
+    public string Username => _authService.Username;
+    public bool IsAdmin => _authService.IsAdmin;
+    public bool isAuthenticated => _authService.IsAuthenticated;
+    public IndexModel(NetMonitorContext db, AuthService authService)
     {
         _db = db;
+        _authService = authService;
     }
+
+    public List<MonitorInstanceDto> MonitorInstances { get; private set; } = new();
 
     public IActionResult OnGet()
     {
         var monitorInstance = _db.MonitorInstances
             .Select(m => new MonitorInstanceDto(m.Name,
                 m.Hosts.Select(h => new HostDto(h.Guid, h.Hostname, h.Description.description,
-                    h.Description.longdescription, h.IPAddress, new List<ServiceDto>())).ToList(), m.Guid)).ToList();
+                    h.Description.longdescription, h.IPAddress, new List<ServiceDto>())).ToList(), m.Guid, m.Manager)).ToList();
         if (monitorInstance is null) return RedirectToPage("/");
         MonitorInstances = monitorInstance;
         return Page();
